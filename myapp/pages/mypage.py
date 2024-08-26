26616cb9e890a4c9b870f34a876c333a95122411
tomatolian <vinegtom@gmail.com>
2024-08-26 13:10:18 +0900

    add chat page

---
+++ myapp/pages/mypage.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("""## My Profile
+
+my name is **クラッシャーバンバンFuji**
+
+my favorite programming language is <strong>Python</strong>
+
+
+My Special moves are F-U and DDT.
+
+Streamlit is awesome
+
+Here is Streamlit's [official website](https://streamlit.io/)
+""")
---
+++ myapp/pages/search_nippo.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("""## About
+This WebApp is created by **あああ**.
+This WebApp is created for the purpose of learning Streamlit.
+I hope you enjoy this WebApp.
+""")
