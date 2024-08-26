c8c8e0dcebabce7cc973a2227a2271c18c7d4e2f
tomatolian <vinegtom@gmail.com>
2024-08-26 16:05:46 +0900

    fix bug

--- myapp/pages/chatpage.py
+++ myapp/pages/chatpage.py
@@ -9,7 +9,7 @@ else:
 # 定数定義
 USER_NAME = "user"
 ASSISTANT_NAME = "assistant"
-
+MORIAGE_YAKU_NAME ="moriage"
 # チャットログを保存したセッション情報を初期化
 if "chat_log" not in st.session_state:
     st.session_state.chat_log = []
