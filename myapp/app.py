6101624fe0656201037f871008af64dcfccf2584
Ryo Sakai <rsakai@Ryos-Air.lan>
2024-08-26 16:46:13 +0900

    able to fetch data from mongodb

--- myapp/app.py
+++ myapp/app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+from pymongo import MongoClient
 
-# st.write()はMarkdown表記対応
-st.write('# headline300')
-# 以下のように明示的に書くことも可能
-st.markdown('# headline2')
+# Connect to MongoDB
+client = MongoClient("mongodb://root:password@mongodb:27017/")
+db = client["mydb"]  # Replace with your database name
+collection = db["mycollection"]  # Replace with your collection name
+
+# Fetch data from MongoDB
+documents = list(collection.find())
+
+# Display data in Streamlit
+st.title("MongoDB Data")
+
+if documents:
+    for doc in documents:
+        st.write(doc)
+else:
+    st.write("No data found.")
+
+# Close the connection
+client.close()
+
