936de505457c0b0db2f4856dc238bb5ce1a2993e
tomatolian <vinegtom@gmail.com>
2024-08-26 14:45:09 +0900

    add calendar view

--- myapp/pages/mypage.py
+++ myapp/pages/mypage.py
@@ -1,15 +1,50 @@
+
 import streamlit as st
+from streamlit_calendar import calendar
+import json
 
-st.markdown("""## My Profile
+calendar_options = {
+    "selectable": True,
+}
 
-my name is **クラッシャーバンバンFuji**
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2024-08-26T08:30:00",
+        "end": "2024-08-26T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2024-08-26T07:30:00",
+        "end": "2024-08-26T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2024-08-26T10:40:00",
+        "end": "2024-08-26T12:30:00",
+        "resourceId": "a",
+    }
+]
 
-my favorite programming language is <strong>Python</strong>
+custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
+    }
+    .fc-event-time {
+        font-style: italic;
+    }
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+"""
 
-
-My Special moves are F-U and DDT.
-
-Streamlit is awesome
-
-Here is Streamlit's [official website](https://streamlit.io/)
-""")
+calendar = calendar(events=calendar_events, options=custom_css, custom_css=custom_css, callbacks=['dateClick', 'eventClick', 'eventChange', 'eventsSet', 'select'], license_key='CC-Attribution-NonCommercial-NoDerivatives', key=None)
+if calendar.get("eventClick"):
+    event_data = calendar["eventClick"]["event"]
+    st.dialog(f"イベントがクリックされました: {event_data['title']} 開始時間: {event_data['start']}")
+    st.write(f"イベントがクリックされました: {event_data['title']} 開始時間: {event_data['start']}")
