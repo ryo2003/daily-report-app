e689c26ace57eb9709a9c25699791abeaa65df50
helvetica <ayakasakai.tsukuba@gmail.com>
2024-08-27 08:28:00 +0900

    add login page

--- myapp/app.py
+++ myapp/app.py
@@ -8,5 +8,6 @@ my_profile = st.Page(page="pages/mypage.py", title="mypage", icon=":material/ope
 search = st.Page(page="pages/search_nippo.py", title="search", icon=":material/home:")
 chat = st.Page(page="pages/chatpage.py", title="chat", icon=":material/home:")
 create_by_hand = st.Page(page="pages/createbyhands.py",title = "手動で日報作成")
-pg = st.navigation([top_page, my_profile,search,chat,create_by_hand])
+login = st.Page(page="pages/login.py",title = "login")
+pg = st.navigation([top_page, my_profile,search,chat,create_by_hand,login])
 pg.run()
--- myapp/pages/login.py
+++ myapp/pages/login.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-st.title("日報管理システムにログインする")
+# タイトル
+st.title('日報管理システムにログインする')
+
+st.write("現在はID=abc ,パスワード=123456のみ対応しています")
+# ログインIDの入力ボックス
+login_id = st.text_input('ログインID:')
+
+# パスワードの入力ボックス
+password = st.text_input('パスワード:', type='password')
+
+# ログインボタン
+if st.button('ログイン'):
+    # ログイン処理の部分（例: IDとパスワードのチェックなど）
+    if login_id == "abc" and password == "123456": 
+        #実際にはdb内にあるかを判定する
+        st.success("ログイン成功!")
+        st.switch_page("pages/toppage.py")
+    else:
+        st.error("ログインIDまたはパスワードが間違っています。")
