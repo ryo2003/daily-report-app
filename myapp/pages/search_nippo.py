fa38d7392c48870c70527fb84422e35e11178606
Ryo Sakai <rsakai@Ryos-Air.lan>
2024-08-28 16:56:38 +0900

    done searching by name

--- myapp/pages/search_nippo.py
+++ myapp/pages/search_nippo.py
@@ -1,85 +1,80 @@
 import streamlit as st
 import pandas as pd
-
-# 仮の日報データ
-# データベースができたらそっちから引っ張る
-data = {
-    "報告者": ["山田太郎", "佐藤花子", "鈴木一郎"],
-    "企業名": ["株式会社A", "株式会社B", "株式会社C"],
-    "訪問時間": ["2024-08-20 10:00", "2024-08-21 14:00", "2024-08-22 09:00"],
-    "訪問目的": ["提案", "フォロー", "クローズ"],
-    "お客様の課題": ["価格競争が激しい", "納期の短縮", "競合他社が強力"],
-}
-
-# データフレームに変換
-df = pd.DataFrame(data)
-
-st.title("日報検索")
-
-# 検索フォーム
-st.sidebar.header("検索条件")
-selected_name = st.sidebar.selectbox("報告者を選択してください", options=["すべて"] + list(df["報告者"].unique()))
-selected_company = st.sidebar.selectbox("企業名を選択してください", options=["すべて"] + list(df["企業名"].unique()))
-selected_purpose = st.sidebar.selectbox("訪問目的を選択してください", options=["すべて"] + list(df["訪問目的"].unique()))
-
-# 検索ボタン
-search_button = st.sidebar.button("検索")
-
-#selected_name/company/purposeをutilsの検索functionに入れる
-if search_button :
-    st.write("報告者："+selected_name+" 企業名："+selected_company+" 訪問目的:"+selected_purpose+"　での検索結果は以下です")
-    st.write("まだ検索用の関数が作られていないためエラーがでます")
-    search_result = utils.search(selected_name,selected_company,selected_purpose)
-    st.write(search_result)
-
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/app/utils/')))
 from data_fetch import get_nippo, get_username, get_user, get_client, init_database, fetch_nippo_async
 from bson import ObjectId
 
+# 仮の日報データ
+# データベースができたらそっちから引っ張る
+users = []
+customers = []
+purposes = []
+
+st.title("日報検索")
+
 st.title("Feed View Example")
 
 import streamlit as st
 import asyncio
-
-
-
-# # Define Document models for nippo and user collections
-# class Nippo(Document):
-#     user_id: PydanticObjectId
-#     contents: str
-#     good: list
-#     bookmark: list
-#     purpose: str
-#     customer: str
-
-#     class Settings:
-#         name = "nippo"
-
-
-# class User(Document):
-#     name: str
-
-
-
-# Main async function to run the app
-async def main():
-    client = get_client()
-    await init_database(client)
-    
-    nippo_data = await fetch_nippo_async()
-
-    st.write("Nippo Data:")
-    # for nippo in nippo_data:
-    #     st.write(nippo.purpose)
-
+def select_nippo(nippos,sel_username):
     result_str = '<html><table style="border: none; width: 100%;">'
+    for nippo in nippos:
+        if(get_username(nippo.user_id) == sel_username):
+            username = get_username(nippo.user_id)
+            users.append(username)
+            purpose = nippo.purpose
+            purposes.append(purpose)
+            customer = nippo.customer
+            customers.append(customer)
+            src_time = nippo.get("src_time", "Unknown time")
+            
+            result_str += f'<tr style="border: none; background-color: whitesmoke; margin-bottom: 15px;">'
+            result_str += f'<td style="border: none; padding: 10px;">'
 
-    for nippo in nippo_data:
+            # Display username
+            result_str += f'<div style="font-size: 16px; color: dimgray; margin-top: 5px;">Username: {username}</div>'
+            
+            
+            # Display purpose
+            result_str += f'<div style="font-size: 16px; color: dimgray; margin-top: 5px;">Purpose: {purpose}</div>'
+            
+            # Display customer
+            result_str += f'<div style="font-size: 16px; color: dimgray; margin-top: 5px;">Customer: {customer}</div>'
+            
+            # Display time
+            result_str += f'<div style="font-size: 12px; color: green; margin-top: 10px;">{src_time}</div>'
+            
+            result_str += f'</td></tr>'
+            
+            # Spacer row
+            result_str += f'<tr style="border: none;"><td style="border: none; height: 10px;"></td></tr>'
+
+    result_str += '</table></html>'
+
+    # Hide Streamlit's menu and footer
+    hide_streamlit_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        .css-hi6a2p {padding-top: 0rem;}
+        </style>
+        """
+
+    # Render the result in Streamlit
+    st.markdown(result_str, unsafe_allow_html=True)
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+def show_nippo(nippos, sel_username=None):
+    result_str = '<html><table style="border: none; width: 100%;">'
+    for nippo in nippos:
         username = get_username(nippo.user_id)
+        users.append(username)
         purpose = nippo.purpose
+        purposes.append(purpose)
         customer = nippo.customer
+        customers.append(customer)
         src_time = nippo.get("src_time", "Unknown time")
         
         result_str += f'<tr style="border: none; background-color: whitesmoke; margin-bottom: 15px;">'
@@ -117,6 +112,47 @@ async def main():
     # Render the result in Streamlit
     st.markdown(result_str, unsafe_allow_html=True)
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Main async function to run the app
+async def main():
+
+    client = get_client()
+    await init_database(client)
+    
+    nippo_data = await fetch_nippo_async()
+
+    st.write("Nippo Data:")
+
+
+    show_nippo(nippo_data)
+    
+
+    #st.write(users)
+
+    data = {
+    "報告者": users,
+    "企業名": customers,
+    "訪問時間": ["2024-08-20 10:00", "2024-08-21 14:00", "2024-08-22 09:00"],
+    "訪問目的": purposes,
+    "お客様の課題": ["価格競争が激しい", "納期の短縮", "競合他社が強力"],
+    }
+
+    # 検索フォーム
+    st.sidebar.header("検索条件")
+    selected_name = st.sidebar.selectbox("報告者を選択してください", options=["すべて"] + data.get("報告者"))
+    selected_company = st.sidebar.selectbox("企業名を選択してください", options=["すべて"] + data.get("企業名"))
+    selected_purpose = st.sidebar.selectbox("訪問目的を選択してください", options=["すべて"] + data.get("訪問目的"))
+
+    # 検索ボタン
+    search_button = st.sidebar.button("検索")
+
+    #selected_name/company/purposeをutilsの検索functionに入れる
+    if search_button :
+        st.write("報告者："+selected_name+" 企業名："+selected_company+" 訪問目的:"+selected_purpose+"　での検索結果は以下です")
+        st.write("まだ検索用の関数が作られていないためエラーがでます")
+        select_nippo(nippo_data,selected_name)
+        #search_result = utils.search(selected_name,selected_company,selected_purpose)
+        #st.write(search_result)
     
 
 
