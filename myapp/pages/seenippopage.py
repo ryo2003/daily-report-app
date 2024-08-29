b5900a664c4eb3c5862ed5e799745b0fb6005412
Ryo Sakai <rsakai@Ryos-Air.lan>
2024-08-29 16:17:10 +0900

    seenippopage

--- myapp/pages/seenippopage.py
+++ myapp/pages/seenippopage.py
@@ -3,48 +3,60 @@ from bson import ObjectId
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/app/utils/')))
-from data_fetch import getnippo_from_nippoid, get_username
-from streamlit_text_rating.st_text_rater import st_text_rater
+#from data_fetch import getnippo_from_nippoid, get_username
 
-# ログインしているユーザのid取得
-userid = st.session_state.get("success_id")
+import streamlit as st
 
-# 閲覧している日報のid取得(仮)
-nippo_id = ObjectId("66cee55308302a8a9e19a148")
+# Retrieve the nippo_id from the query parameters or session state
+#query_params = st.experimental_get_query_params()
 
-# nippo_idからnippoデータを取得
-nippo = getnippo_from_nippoid(nippo_id)
+value = st.session_state.get("success_id")
+nippo_id = st.session_state.get("selected_nippo_id")
+st.write(value)
+st.write(nippo_id)
 
-# nippo_idの日報の作成者をユーザID取得
-author_id = nippo["user_id"]
-author_username = get_username(author_id)
 
-# nippo_idからeventidを取得して、イベント名を取得するコードを書く必要あり
-# 現在は仮のイベント名を入力
-event_name = "A会社との商談"
+#from streamlit_text_rating.st_text_rater import st_text_rater
 
-st.title(f"イベント: {event_name} の日報")
-st.write(f"作成者: {author_username}")
-contents = nippo["contents"]
+# # ログインしているユーザのid取得
+# userid = st.session_state.get("success_id")
 
-# シンプルな列挙形式で表示
-st.subheader("日報の内容")
-for key, value in contents.items():
-    st.write(f"- **{key}**: {value}")
+# # 閲覧している日報のid取得(仮)
+# nippo_id = ObjectId("66cee55308302a8a9e19a148")
 
-# カスタム評価ウィジェットの呼び出し
-text = "この日報は役に立ちましたか？"
-response = st_text_rater(text=text)
+# # nippo_idからnippoデータを取得
+# nippo = getnippo_from_nippoid(nippo_id)
 
-# セッション状態の更新
-if response == "liked":
-    st.session_state["response"] = response
-elif response == "disliked":
-    st.session_state["response"] = response
+# # nippo_idの日報の作成者をユーザID取得
+# author_id = nippo["user_id"]
+# author_username = get_username(author_id)
 
-# ボタンが押されたらメッセージを表示
-if st.session_state.get("response") == "liked":
-    st.write("いいねしました")
-elif st.session_state.get("response") == "disliked":
-    st.write("バッドしました")
+# # nippo_idからeventidを取得して、イベント名を取得するコードを書く必要あり
+# # 現在は仮のイベント名を入力
+# event_name = "A会社との商談"
+
+# st.title(f"イベント: {event_name} の日報")
+# st.write(f"作成者: {author_username}")
+# contents = nippo["contents"]
+
+# # シンプルな列挙形式で表示
+# st.subheader("日報の内容")
+# for key, value in contents.items():
+#     st.write(f"- **{key}**: {value}")
+
+# # カスタム評価ウィジェットの呼び出し
+# text = "この日報は役に立ちましたか？"
+# response = st_text_rater(text=text)
+
+# # セッション状態の更新
+# if response == "liked":
+#     st.session_state["response"] = response
+# elif response == "disliked":
+#     st.session_state["response"] = response
+
+# # ボタンが押されたらメッセージを表示
+# if st.session_state.get("response") == "liked":
+#     st.write("いいねしました")
+# elif st.session_state.get("response") == "disliked":
+#     st.write("バッドしました")
 
