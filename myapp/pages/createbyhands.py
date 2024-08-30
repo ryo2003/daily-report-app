337b82a2ae14f21d1f37ddbe1782523a8ff28061
helvetica <ayakasakai.tsukuba@gmail.com>
2024-08-30 10:51:04 +0900

    made byhands submit don't cause double nippo

--- myapp/pages/createbyhands.py
+++ myapp/pages/createbyhands.py
@@ -2,10 +2,9 @@ import streamlit as st
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/app/utils/')))
-from data_register import submit_byhands_new
+from data_register import submit_byhands_new,nippo_exist
 from bson import ObjectId
 from datetime import datetime
-
 userid = st.session_state.get("success_id")
 
 event_data = st.session_state.get("event_data")
@@ -43,13 +42,15 @@ if st.session_state.get('getconsent'):
         st.write("企業名を入力してください")
     else:
             
-        st.write("以下の内容を本当に送信しますか？")
+        st.write("以下の内容を送信します")
         st.write(f"企業名: {nippo_temporary['企業名']}")
         st.write(f"訪問時間: {nippo_temporary['訪問時間']}")
         st.write(f"訪問目的: {nippo_temporary['訪問目的']}")
         st.write(f"本文: {nippo_temporary['本文']}")
-
+        exist_id = nippo_exist(event_data)
+        if exist_id != None:
+            st.warning("日報が既に存在しています。送信するボタンを押すと日報が上書きされます。")
 
         if st.button("送信する"):
-            submit_byhands_new(nippo_temporary,userid,event_data)
-            st.write("送信しました")
+            submit_byhands_new(nippo_temporary,userid,event_data,exist_id)
+            st.write("日報を送信しました")
